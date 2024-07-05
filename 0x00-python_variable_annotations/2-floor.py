4a652f0d995cddf72061b17f46b26b346422f90a
Vulcanric <jneric49@gmail.com>
2024-07-05 02:42:28 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Defines a function, `floor`"""
+
+
+def floor(n: float) -> int:
+    """
+    Function floor: Computes the floor of a floating-point number
+        :param n: Floating point number
+        Returns: Floor of the floating point number
+    """
+    return int(n // 1)
