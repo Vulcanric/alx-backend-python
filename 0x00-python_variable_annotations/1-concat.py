316d412ab555a8dc1faf9428011e9c1aa7edcacf
Vulcanric <jneric49@gmail.com>
2024-07-05 02:29:35 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module defines a function: `concat`"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function concat:
+        Concatenates two strings
+        :param str1: first string
+        :param str2: second string
+        Returns: The concatenated string
+    """
+    return str1 + str2
