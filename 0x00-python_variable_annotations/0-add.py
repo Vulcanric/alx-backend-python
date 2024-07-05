fb7f0f41b0f4d5c4a6740a2b4a2354ed0c5cfe4d
Vulcanric <jneric49@gmail.com>
2024-07-05 02:04:40 +0100

    Basic annotations - add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+"""This module defines a type-annotated function: `add`"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add: Returns the sum of two floats
+    :param a: first floating point number
+    :param b: second floating point number
+    Returns: Their sum
+    """
+    return a + b
