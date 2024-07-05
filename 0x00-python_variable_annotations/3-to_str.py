08e75863d1ab4b4c27c02b4a759a7662b4f6bad4
Vulcanric <jneric49@gmail.com>
2024-07-05 02:58:37 +0100

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Defines a function, `to_str`"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function to_str - Converts a float to a string
+        :param n: Number to convert
+        Returns: the string representation of param n
+    """
+    return str(n)
