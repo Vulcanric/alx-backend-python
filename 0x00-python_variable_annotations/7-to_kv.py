7bff960a272c968b7cff532a3739b4810c0f7c0f
Vulcanric <jneric49@gmail.com>
2024-07-05 03:59:53 +0100

    Fix: added Union to return type

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ and an int Or float `v` as argument and returns a tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
     """
     Function to_kv:
         Returns a tuple containing (k, v)
