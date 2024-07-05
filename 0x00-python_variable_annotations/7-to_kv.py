16c8258ccfdcaf781a3ce15c739b987340092f07
Vulcanric <jneric49@gmail.com>
2024-07-05 03:57:22 +0100

    Implemented even more complex types - string and int/float to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Defines a type-annotated function, `to_kv` that takes a string `k`
+and an int Or float `v` as argument and returns a tuple
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Function to_kv:
+        Returns a tuple containing (k, v)
+    """
+    return (k, v)
