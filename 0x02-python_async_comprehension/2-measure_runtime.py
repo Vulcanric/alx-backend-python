5e0e6616a58cae4902380dbb49eb653078d1127b
Vulcanric <jneric49@gmail.com>
2024-07-23 16:01:50 +0000

    Measures runtime for four parallel comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Run time for four parallel comprehensions """
+import asyncio
+import time
+
+
+async def measure_runtime() -> float:
+    """
+    Executes async_comprehension() from 1-async_comprehensions.py, four times
+    in parallel
+
+    Args:
+        None
+
+    Returns:
+        Measure of runtime
+    """
+    async_comprehension = __import__('1-async_comprehension').\
+            async_comprehension
+
+    start = time.perf_counter()
+    asyncio.gather(*(async_comprehension() for _ in range(4)))
+    stop = time.perf_counter()
+
+    return stop - start
