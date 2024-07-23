6bcff6eb8e19c1103be0639450b4ef5c92a878d1
Vulcanric <jneric49@gmail.com>
2024-07-23 05:08:10 +0000

    Asynchronous generators

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -17,6 +17,6 @@ async def async_generator() -> AsyncGenerator[float, None, None]:
     """
     i = 0
     while i < 10:
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
         i += 1
